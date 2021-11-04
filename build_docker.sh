ef8c29d9fc343cd720a8757a0800aa65820cc198
Marcio Paduan Donadio <marciodo@gmail.com>
2021-11-03 19:19:26 -0700

    Support to bring packages from the AFS

--- build_docker.sh
+++ build_docker.sh
@@ -2,22 +2,24 @@
 
 temp_docker_file=Dockerfile_temp
 release_site_template=RELEASE_SITE.template
+temp_files_dir=temp_files
 git_repos=/afs/slac/g/cd/swe/git/repos/package/epics
+package_area=/afs/slac/g/lcls/package
 
 # $1 is the EPICS base version
 function base_version_in_files() {
     sed -i -e "s/*TAG_BASE_VERSION/$1/g" $temp_docker_file
-    cp -f $release_site_template epics/modules/RELEASE_SITE
-    sed -i -e "s/*TAG_BASE_VERSION/$1/g" epics/modules/RELEASE_SITE
+    cp -f $release_site_template $temp_files_dir/modules/RELEASE_SITE
+    sed -i -e "s/*TAG_BASE_VERSION/$1/g" $temp_files_dir/modules/RELEASE_SITE
 }
 
 # $1 is the EPICS base version to bring
 function bring_epics_base() {
-    mkdir -p epics/base
+    mkdir -p $temp_files_dir/base
     # Remove last portion of the version number and replace with the string
     # 'branch'. We are cloning the branch HEAD instead of a tag.
     base_branch=${base_version%?}branch
-    git clone --branch $base_branch $git_repos/base/base.git epics/base/$1
+    git clone --branch $base_branch $git_repos/base/base.git $temp_files_dir/base/$1
     base_version_in_files $1
 }
 
@@ -31,7 +33,7 @@ function add_module_to_dockerfile() {
     echo WORKDIR \${EPICS_MODULES} >> $temp_docker_file
     echo RUN mkdir -p $1/\${${1^^}_MODULE_VERSION} >> $temp_docker_file
     echo WORKDIR $1/\${${1^^}_MODULE_VERSION} >> $temp_docker_file
-    echo COPY epics/modules/$1/\${${1^^}_MODULE_VERSION} . >> $temp_docker_file
+    echo COPY $temp_files_dir/modules/$1/\${${1^^}_MODULE_VERSION} . >> $temp_docker_file
 #    echo WORKDIR \${${1^^}_MODULE_VERSION} >> $temp_docker_file
     echo "#" Point to the re2c install in the system >> $temp_docker_file
     echo RUN if [ -f configure/CONFIG_SITE ]";" then sed -i -e "'s|^RE2C =.*|RE2C = /usr/bin/re2c|g'" configure/CONFIG_SITE";" fi >> $temp_docker_file
@@ -45,13 +47,13 @@ function add_module_to_dockerfile() {
 # $1 is the module name.
 # $2 is the module version.
 function bring_module() {
-    mkdir epics/modules/$1
-    git clone --branch $2 $git_repos/modules/$1.git epics/modules/$1/$2
+    mkdir $temp_files_dir/modules/$1
+    git clone --branch $2 $git_repos/modules/$1.git $temp_files_dir/modules/$1/$2
     add_module_to_dockerfile $1 $2
 }
 
 function bring_epics() {
-    mkdir -p epics/modules
+    mkdir -p $temp_files_dir/modules
     filename='epics-modules'
 
     # Reading each line with modules versions.
@@ -75,11 +77,55 @@ function bring_epics() {
     done < $filename
 }
 
-function bring_packages() {
+# $1 is the package name and version.
+# $2 is the file line number where to add the info.
+function add_package_to_dockerfile() {
+    # Add text at the right position:
+    sed_command="$(echo $package_line"iCOPY "$temp_files_dir/packages/$1 $1)"
+    sed -i "$sed_command" $temp_docker_file
+}
 
+function bring_packages() {
+    #mkdir -p $temp_files_dir/packages
+    filename='packages'
+
+    # Get the line with the text *TAG_POSITION_FOR_PACKAGES in Dockerfile_base.
+    # This is where we are going to create the commands to copy the packages
+    # inside the container.
+    package_line="$(grep -wn *TAG_POSITION_FOR_PACKAGES Dockerfile_base | cut -d: -f1)"
+
+    # Delete tag line
+    sed -i "$package_line""d" $temp_docker_file
+
+    # Reading each line with package versions.
+    while read line; do
+	# Read last string after space. It must be in the format
+	# <module name>/<version>
+        #version=${line##* }
+
+	# Separates the module name from the version in an array
+        #tuplet=(${version//// })
+
+	#if [ ${tuplet[0]} == 'base' ]; then
+        #    base_version=${tuplet[1]}
+
+            # Bring EPICS base from AFS Git
+	#    bring_epics_base $base_version
+	#else
+	    # Bring the module from AFS Git
+	#    bring_module ${tuplet[0]} ${tuplet[1]}
+	#fi
+
+	echo Copying $line
+	mkdir -p $temp_files_dir/packages/$line
+	cp -Rn $package_area/$line/rhel6-x86_64 $temp_files_dir/packages/$line
+
+	add_package_to_dockerfile $line $package_line
+    done < $filename
 }
 
 cp -f Dockerfile_base $temp_docker_file
+bring_packages
 bring_epics
 cp -f $temp_docker_file Dockerfile
 
